dafa56680a63971a20b51cee40cf62273f748ada
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-25 15:23:19 +0530

    commit

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-
+import { images, data } from "../../constants";
 import "./SpecialMenu.css";
 
 const SpecialMenu = () => <div>SpecialMenu</div>;
 
 export default SpecialMenu;
-
-//Chef Done- Special Menu Left
