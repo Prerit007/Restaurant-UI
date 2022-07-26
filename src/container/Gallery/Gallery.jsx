432f153ab0e4ad435a8c1dee1b704ee834ef0a69
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-26 10:05:55 +0530

    commit

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -4,12 +4,20 @@ import {
   BsArrowLeftShort,
   BsArrowRightShort,
 } from "react-icons/bs";
-import "./Gallery.css";
+
 import { SubHeading } from "../../components";
 import { images } from "../../constants";
+import "./Gallery.css";
 
 const Gallery = () => {
   const scrollRef = useRef(null);
+
+  const img = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04,
+  ];
 
   const scroll = (direction) => {
     const { current } = scrollRef;
@@ -21,37 +29,35 @@ const Gallery = () => {
     }
   };
 
-  const img = [
-    images.gallery01,
-    images.gallery02,
-    images.gallery03,
-    images.gallery04,
-  ];
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
-        <SubHeading title="Photogram" id="gallery" />
+        <SubHeading title="Photogram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
-          bbcscsbscibsicis Lorem ipsum
+        <p
+          className="p__opensans"
+          style={{ color: "#AAAAAA", marginTop: "2rem" }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
         </p>
         <button type="button" className="custom__button">
           View More
         </button>
       </div>
       <div className="app__gallery-images">
-        <div className="app__gallery-images-container" ref={scrollRef}>
+        <div className="app__gallery-images_container" ref={scrollRef}>
           {img.map((image, index) => (
             <div
               className="app__gallery-images_card flex__center"
               key={`gallery_image-${index + 1}`}
             >
-              <img src={image} alt="gallery" />
+              <img src={image} alt="gallery_image" />
               <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
-        <div className="app__gallery-images_arrow">
+        <div className="app__gallery-images_arrows">
           <BsArrowLeftShort
             className="gallery__arrow-icon"
             onClick={() => scroll("left")}
