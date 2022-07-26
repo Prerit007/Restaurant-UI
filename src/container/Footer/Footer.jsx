632662e029cf798cbadb97e9b7f3074c44a75e04
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-26 10:20:56 +0530

    changes

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-
+import { FiFacebook, FiInstagram, FiTwitter } from "react-icons/fi";
 import "./Footer.css";
 import { FooterOverlay, Newsletter } from "../../components";
 
 const Footer = () => (
-  <div>
+  <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
+    <FiInstagram />
+    <FiFacebook />
+    <FiTwitter />
   </div>
 );
 
