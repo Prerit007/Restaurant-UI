37d630ae0b903c6b692941950fe1ad32d8121574
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-26 10:55:58 +0530

    commit

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -7,9 +7,21 @@ const Footer = () => (
   <div className="app__footer section__padding">
     <FooterOverlay />
     <Newsletter />
-    <FiInstagram />
-    <FiFacebook />
-    <FiTwitter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+      </div>
+      <div className="app__footer-links_logo">
+        <h1 className="headtext__comrant">Gericht</h1>
+        <FiInstagram />
+        <FiFacebook />
+        <FiTwitter />
+      </div>
+      <div className="app__footer-links_work"></div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Gericht. All rights reserved.</p>
+    </div>
   </div>
 );
 
